4c1dc36db2712838458dced76550029a1e151292
Trần Lê Nhật Tân <ntan15481@gmail.com>
2026-05-17 11:21:54 +0700

    Update code_tetris.cpp
    sửa lại ban đầu tại commit lộn branch

--- code_tetris.cpp
+++ code_tetris.cpp
@@ -300,7 +300,8 @@ public:
  
 // ===================== GAME STATE =====================
 int px = 4, py = 0;      // v? trí kh?i hi?n t?i
-Piece* curPiece = nullptr;          
+int curBlock = 0;
+int curRot = 0;
 int score = 0;
 bool gameOver = false;
  
@@ -318,22 +319,10 @@ void fillBag() {
     bagIdx = 0;
 }
  
-Piece* createPiece(int type) {
-    switch (type) {
-    case 0: return new IPiece();
-    case 1: return new OPiece();
-    case 2: return new TPiece();
-    case 3: return new SPiece();
-    case 4: return new ZPiece();
-    case 5: return new JPiece();
-    case 6: return new LPiece();
-    }
-    return nullptr;
-}
 
-Piece* nextBlock() {
+int nextBlock()() {
     if (bagIdx >= 7) fillBag();
-    return createPiece(bag[bagIdx++]);
+    return bag[bagIdx++];
 }
  
 // ===================== HELPERS =====================
@@ -475,21 +464,14 @@ void removeLines() {
  
 // ===================== SPAWN =====================
 bool spawnBlock() {
-
-    if (curPiece) delete curPiece;
-
-    curPiece = nextBlock();
-
+    curBlock = nextBlock();
+    curRot = 0;
     px = W / 2 - 2;
     py = 0;
-
-    curPiece->rot = 0;
-
-    if (!canPlace(curPiece, curPiece->rot, px, py)) {
+    if (!canPlace(curBlock, curRot, px, py)) {
         gameOver = true;
         return false;
     }
-
     return true;
 }
  
@@ -589,6 +571,5 @@ int main() {
  
     if (gameOver) showGameOver();
     setColor(7);
-    if (curPiece) delete curPiece;
     return 0;
 }
